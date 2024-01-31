c2566ea8d1c046fedcca7c8dc26cfe940b625c2a
Alex Manley <amanley97@ku.edu>
2024-01-31 13:24:22 -0600

    added user data handling

--- api/obtain-api.py
+++ api/obtain-api.py
@@ -12,7 +12,7 @@
 #         Mahmudul Hasan (m.hasan@ku.edu)
 # ----------------------------------------------------------------------------
 
-import threading, os, signal, multiprocessing
+import os, multiprocessing
 from gem5.components.processors.cpu_types import *
 from gem5.components.memory import *
 from gem5.components import *
@@ -31,8 +31,6 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import inspect, json
 
 MemTypes = {name:obj for name,obj in inspect.getmembers(dram_interfaces, inspect.ismodule)}
-# Global flag to control server shutdown
-shutdown_flag = False
 
 def get_cls(file, mask):
     cls = []
@@ -70,6 +68,8 @@ class SimpleHandler(BaseHTTPRequestHandler):
             self.handle_run_simulator()
         elif self.path == '/shutdown':
             self.handle_shutdown()
+        elif self.path == '/user-data':
+            self.handle_user_data()
         else:
             self.send_error(404, 'Not Found')
 
@@ -86,26 +86,41 @@ class SimpleHandler(BaseHTTPRequestHandler):
         self.wfile.write(get_cpu_types())
 
     def handle_run_simulator(self):
-        # simulator_thread = threading.Thread(target=run_gem5_simulator())
-        # simulator_thread.start()
         print("PRESSED SIMULATION")
         process = multiprocessing.Process(target=run_gem5_simulator)
         process.start()
         process.join()
-        # run_gem5_simulator()
+        
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b"Simulator started in a separate thread\n")
 
     def handle_shutdown(self):
-        global shutdown_flag
-        shutdown_flag = True  # Set the global flag to initiate shutdown
+        # TODO: Ensure the backend server and port are closed
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b"Shutting down server\n")
+
+    def handle_user_data(self):
+        try:
+            content_length = int(self.headers['Content-Length'])
+            data = self.rfile.read(content_length)
+            received_data = json.loads(data.decode('utf-8'))
+            response_data = {"received_data": received_data}
+            print(response_data)
+            # Send a JSON response
+            self.send_response(200)
+
+        except Exception as e:
+            # Handle any exceptions that might occur during processing
+            print(f"Error processing PUT request: {e}")
+            self.send_response(500, {"error": "Internal Server Error"})
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"User data recieved.\n")
 
 #=============================================================#
 
@@ -113,25 +128,8 @@ def run_server(port=5000):
     server_address = ('', port)
     httpd = HTTPServer(server_address, SimpleHandler)
 
-    # Run the server in a separate thread
-    # server_thread = threading.Thread(target=httpd.serve_forever)
-    # server_thread.start()
-
-
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
-    # Keep the main thread busy (e.g., waiting for user input)
-    # while not shutdown_flag:
-    #     pass
-    #
-    # # Close the server's socket explicitly before initiating shutdown
-    # print("Closing server socket...")
-    # httpd.server_close()
-    #
-    # # Initiate graceful shutdown
-    # print("Shutting down server...")
-    # httpd.shutdown()
-    # server_thread.join()  # Wait for the server thread to finish
 
 def run_gem5_simulator():
         print("PID: ", os.getpid())
@@ -140,12 +138,12 @@ def run_gem5_simulator():
 
 board = SimpleBoard(
     clk_freq="3GHz",
-    processor=SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=1),
+    processor=SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.ARM, num_cores=1),
     memory=SingleChannelDDR3_1600(size="32MB"),
     cache_hierarchy=NoCache()
 )
 board.set_se_binary_workload(
-    obtain_resource("x86-hello64-static")
+    obtain_resource("arm-hello64-static")
 )
 
 if __name__ == '__m5_main__':
