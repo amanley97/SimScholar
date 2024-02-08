a443c52f08c7a8827335af905785c85febbd2d6a
Alex Manley <amanley97@ku.edu>
2024-02-08 16:56:20 -0600

    add proprietary info

--- frontend/debug.py
+++ frontend/debug.py
@@ -1,3 +1,17 @@
+# ----------------------------------------------------------------------------
+# NOTICE: This code is the exclusive property of University of Kansas
+#         Architecture Research and is strictly confidential.
+#
+#         Unauthorized distribution, reproduction, or use of this code, in
+#         whole or in part, is strictly prohibited. This includes, but is
+#         not limited to, any form of public or private distribution,
+#         publication, or replication.
+#
+# For inquiries or access requests, please contact:
+#         Alex Manley (amanley97@ku.edu)
+#         Mahmudul Hasan (m.hasan@ku.edu)
+# ----------------------------------------------------------------------------
+
 import tkinter as tk
 import render
 debug_open = False
