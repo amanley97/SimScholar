abdeae7be027d20eea775047d81e420a08c9b2e5
Alex Manley <amanley97@ku.edu>
2024-02-08 16:54:37 -0600

    latest frontend with new render and debug support

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -13,137 +13,81 @@
 # ----------------------------------------------------------------------------
 
 import tkinter as tk
-from tkinter import ttk
-import calls
+import calls, image, render, debug
+sections = []
 
-# Create the main window
-root = tk.Tk()
-root.title("eager")
+def root_window(en_debug=False):
+    # Create the main window
+    root = tk.Tk()
+    root.title("eager")
 
-# Obtain gem5 data
-options = calls.get_gem5_data()
+    # Obtain gem5 data
+    options = calls.get_gem5_data()
 
-# Define section titles
-section_titles = ["Board Style", "Processor", "Cache Hierarchy", "Memory"]
+    # Define section titles
+    section_menu = {"Board Style" : ["Clock Frequency"], 
+                    "Processor" : ["CPU Type", "Number of Cores"], 
+                    "Cache Hierarchy" : ["L1 Size"], 
+                    "Memory" : ["Size"]
+                    }
 
-# Create dropdown menus, labels, and variables
-section_frames = []
-section_dropdowns = []
-selected_option_labels = []
-section_selected_options = [] 
-current_selected_section = tk.StringVar()
-hint = str("Hints: Some helpful information here")
+    # Create dropdown menus, labels, and variables
+    hint = str("Hints: Some helpful information here")
 
-def run_simulation():
-    hint = calls.http_request("run-simulation", "PUT")
-    bottom_bar.config(text=str(hint.text))
+    def update_select(section, selection):
+        idx = sections.index(section)
+        current_section = sections[idx]
+        current_section["main"]["dropdown_value"] = selection
+        render.hide_section("Cache Hierarchy", sections)
+        render.show_section("Cache Hierarchy", sections)
+        print(current_section)
 
-def exit():
-    calls.http_request("shutdown", "PUT")
-    root.destroy()
+    def edit():
+        # advance_section(True)
+        edit_button.grid_remove()
 
-def edit():
-    advance_section(True)
+    # Header label
+    header_label = tk.Label(root, text="Welcome to EAGER Gem5 GUI", font=('TkDefaultFont', 16, 'bold'))
+    header_label.grid(row=0, column=0, columnspan=3, pady=10)
+
+    for i, (main_option, sub_options) in enumerate(section_menu.items()):
+        # Create each section
+        section = render.render_section(master=root,  
+                                        row_offset=i, 
+                                        title=main_option,
+                                        opts=options[i],
+                                        func= update_select,
+                                        subopts=sub_options
+                                        )
+        sections.append(section)
+
+    # EDIT BUTTON
+    edit_button = tk.Button(root, text="Edit", command=edit)
     edit_button.grid_remove()
 
-def print_selected(list, label):
-    result_dict = {}
-    for label, stringvar in zip(label, list):
-        value = stringvar.get()
-        result_dict[label] = value
-    data_test = calls.http_request("user-data", "PUT", result_dict)
-    print(data_test.text)
+    # CANVAS
+    canvas = tk.Canvas(root, width=600, height=400, bg="white")
+    canvas.grid(row=1, column=1, padx=10, pady=10, rowspan=len(section_menu)*2 + 1, columnspan=2, sticky=tk.W)
 
-def show_section(section, done):
-    # Set weight for the selected section, making it larger
-    selected_index = section_titles.index(section) if not done else len(section_titles) + 1
-    for i in range(len(section_titles)):
-        weight = 1 if i == selected_index else 0
-        root.grid_rowconfigure(i*2 + 1, weight=weight)
+    # HINT BAR
+    bottom_bar = tk.Label(root, text=hint, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+    bottom_bar.grid(row=len(section_menu)*2 + 2, column=0, columnspan=3, padx=5, pady=5, sticky=tk.W+tk.E)
 
-        # Show or hide the dropdown based on the selected section
-        if i == selected_index:
-            section_dropdowns[i].grid(row=0, column=1, padx=5, pady=5)
-            selected_option_labels[i].grid_remove()
-            highlight_color = "black"
-        else:
-            section_dropdowns[i].grid_remove()
-            selected_option_labels[i].grid(row=0, column=2, padx=5, pady=5, sticky=tk.W)
-            selected_option_labels[i].config(text=f"       {section_selected_options[i].get()}")
-            highlight_color = root.cget("bg")
-        
-        section_frames[i].config(highlightthickness=weight*2, highlightbackground=highlight_color)                    
+    if en_debug:
+        # DEBUG BUTTON
+        debug_button = tk.Button(root, text="debug", command=lambda: debug.open_debug(root, sections))
+        debug_button.grid(row=len(section_menu)*2 + 3, column=0, padx=5, pady=5)
 
-    # Set the current selected section
-    current_selected_section.set(section)
+    # SIMULATE BUTTON
+    simulate_button = tk.Button(root, text="Simulate", command=lambda: calls.run_simulation(bottom_bar), width=60)
+    simulate_button.grid(row=len(section_menu)*2 + 3, column=1, padx=10, pady=5, sticky=tk.E)
 
-# Function to advance to the next section
-def advance_section(reset):
-    current_index = section_titles.index(current_selected_section.get())
-    index_count = 0 if reset else current_index + 1
+    # EXIT BUTTON
+    exit_button = tk.Button(root, text="Exit", command=lambda: calls.exit(root=root))
+    exit_button.grid(row=len(section_menu)*2 + 3, column=2, padx=20, pady=5, sticky=tk.E)
 
-    # Save the selected option for the current section
-    section_selected_options[current_index].set(section_dropdowns[current_index].get())
-    # Advance to the next section
-    next_index = (current_index + 1) % len(section_titles)
-    if index_count == len(section_titles):
-        show_section(section_titles[current_index], True)
-        edit_button.grid(row=len(section_titles)*2 + 1, column=0, padx=10, pady=5, sticky=tk.W)
-        print_selected(section_selected_options, section_titles)
-    else:
-        show_section(section_titles[next_index], False)
+    # DEFAULTS
+    canvas.delete("all")
+    root.mainloop()
 
-# Header label
-header_label = tk.Label(root, text="Welcome to EAGER Gem5 GUI", font=('TkDefaultFont', 16, 'bold'))
-header_label.grid(row=0, column=0, columnspan=3, pady=10)
-
-for i, section_title in enumerate(section_titles):
-    # Create a section frame
-    section_frame = tk.Frame(root, highlightthickness=0, relief=tk.RIDGE)
-    section_frame.grid(row=i*2 + 1, column=0, padx=10, pady=5, sticky=tk.W)
-    section_frames.append(section_frame)
-
-    # Add section title to the frame
-    label = tk.Label(section_frame, text=section_title, font=('TkDefaultFont', 12, 'normal'))
-    label.grid(row=0, column=0, padx=5, pady=5)
-
-    # Create a dropdown menu
-    dropdown_var = tk.StringVar()
-    dropdown = ttk.Combobox(section_frame, textvariable=dropdown_var, values=options[i])
-    dropdown.bind("<<ComboboxSelected>>", lambda event=None, : advance_section(False))
-    dropdown.grid(row=0, column=1, padx=5, pady=5)
-    section_dropdowns.append(dropdown)
-
-    # Create a label to display the selected option
-    selected_option_label = tk.Label(section_frame, text="", font=('TkDefaultFont', 10, 'normal'))
-    selected_option_label.grid(row=0, column=2, padx=5, pady=5, sticky=tk.W)
-    selected_option_labels.append(selected_option_label)
-
-    # Create a variable to store the selected option
-    section_selected_options.append(tk.StringVar())
-
-# Testing Edit Button
-edit_button = tk.Button(root, text="Edit", command=edit)
-edit_button.grid_remove()
-
-# Create a larger workspace with Canvas for graphics on the right
-canvas = tk.Canvas(root, width=600, height=400, bg="white")
-canvas.grid(row=1, column=1, padx=10, pady=10, rowspan=len(section_titles)*2 + 1, columnspan=2, sticky=tk.W)
-
-# Create a bottom bar with hints and buttons
-bottom_bar = tk.Label(root, text=hint, bd=1, relief=tk.SUNKEN, anchor=tk.W)
-bottom_bar.grid(row=len(section_titles)*2 + 2, column=0, columnspan=3, padx=5, pady=5, sticky=tk.W+tk.E)
-
-# Add "Simulate" button to the far right below the hint
-simulate_button = tk.Button(root, text="Simulate", command=run_simulation, width=60)
-simulate_button.grid(row=len(section_titles)*2 + 3, column=1, padx=10, pady=5, sticky=tk.E)
-
-# Add "Exit" button to the far right below the hint
-exit_button = tk.Button(root, text="Exit", command=exit)
-exit_button.grid(row=len(section_titles)*2 + 3, column=2, padx=20, pady=5, sticky=tk.E)
-
-# Show Section 1 by default
-show_section("Board Style", False)
-
-# Run the GUI
-root.mainloop()
+root_window(en_debug=True)
