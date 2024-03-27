c3d26221ed9038679702ac532f7ddfdb21f62b32
Alex Manley <amanley97@ku.edu>
2024-03-27 15:51:48 -0500

    test programs for code editor

---
+++ test-progs/hello-v2.c
@@ -0,0 +1,15 @@
+// Example Hello World C Program //
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+int test(int a, int b)
+{
+    int val = a*b;
+    return val;
+}
