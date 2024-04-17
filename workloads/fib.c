e90386e8c24e1e677ca2b7c007aca78c4177e210
Alex Manley <amanley97@ku.edu>
2024-04-17 14:46:47 -0500

    update fib workload

--- workloads/fib.c
+++ workloads/fib.c
@@ -3,17 +3,14 @@
 #include <stdio.h>
 int main() {
 
-  int i, n;
+  int i;
+  int n = 6; //number of terms
 
   // initialize first and second terms
   int t1 = 0, t2 = 1;
 
   // initialize the next term (3rd term)
   int nextTerm = t1 + t2;
-
-  // get no. of terms from user
-  printf("Enter the number of terms: ");
-  scanf("%d", &n);
 
   // print the first two terms t1 and t2
   printf("Fibonacci Series: %d, %d, ", t1, t2);
